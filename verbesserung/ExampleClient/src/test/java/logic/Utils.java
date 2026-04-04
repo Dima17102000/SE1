a1a83ca87c629e5302b169eb69259d4c8398032c
Dima_Odessa <dima_odessa@MacBook-2025-DimaOdessa.local>
2026-04-04 22:21:27 +0200

    Modified StrategyPlannedTourScoreTest and Utils

---
+++ verbesserung/ExampleClient/src/test/java/logic/Utils.java
@@ -0,0 +1,35 @@
+package logic;
+
+import java.util.Set;
+
+import messagesbase.UniquePlayerIdentifier;
+import messagesbase.messagesfromserver.EPlayerGameState;
+import messagesbase.messagesfromserver.FullMap;
+import messagesbase.messagesfromserver.GameState;
+import messagesbase.messagesfromserver.PlayerState;
+
+public class Utils {
+    public static GameHelper generateGameHelper(FullMap map)
+    {
+        GameHelper helper = new GameHelper(new UniquePlayerIdentifier("player1"));
+
+            GameState gameState = new GameState(
+                map,
+                Set.of(
+                    new PlayerState(
+                        "Test",
+                        "Player",
+                        "u123456",
+                        EPlayerGameState.MustWait,
+                        new UniquePlayerIdentifier("player1"),
+                        false
+                    )
+                ),
+                "ABC"
+            );
+
+        helper.update(gameState);
+
+        return helper;
+    }
+}
